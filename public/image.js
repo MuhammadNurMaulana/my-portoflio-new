ec9b8a10da2e10aca17b689200f217700382adf5
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-09-28 20:39:36 +0700

    update fitur

--- public/image.js
+++ public/image.js
@@ -1,3 +1,9 @@
 import my from "./images/my.png";
-
-export { my };
+import html23 from "./images/icons8-html-150.png";
+import css from "./images/icons8-css-150.png";
+import javascript from "./images/icons8-javascript-150.png";
+import bootstrap from "./images/icons8-bootstrap-150.png";
+import tailwind from "./images/icons8-tailwind-css-150.png";
+import sass from "./images/icons8-sass-avatar-150.png";
+import react from "./images/icons8-react-150.png";
+export { my, html23, css, javascript, bootstrap, tailwind, sass, react };
--- src/Components/Skills/index.jsx
+++ src/Components/Skills/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-import skills from "../service/skills.json";
+import { bootstrap, css, html23, javascript, react, sass, tailwind } from "../../../public/image";
 
 export const Skills = () => {
+  const skills = [
+    {
+      name: "HTML5",
+      src: html23,
+    },
+    {
+      name: "CSS3",
+      src: css,
+    },
+    {
+      name: "Javascript",
+      src: javascript,
+    },
+    {
+      name: "Bootstrap",
+      src: bootstrap,
+    },
+    {
+      name: "Tailwind CSS",
+      src: tailwind,
+    },
+    {
+      name: "Sass",
+      src: sass,
+    },
+    {
+      name: "React Js",
+      src: react,
+    },
+  ];
   return (
     <div className="grid grid-cols-2 grid-rows-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mt-10 ">
       {skills.map((skill) => (
